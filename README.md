06e2d7ba0807909a5faa165aaa16a56044b43627
Steven Kao <st.kao.05@gmail.com>
2020-03-17 15:47:31 +0800

    New "gettext-pluralform-placeholder" rule

--- README.md
+++ README.md
@@ -63,6 +63,42 @@ Why: our localization tool parse all code files to look for localized strings, a
 
 
 
+__@doist/doist/gettext-pluralform-placeholder__
 
 
+When localizing string with plural forms, the singular string should not contain the hard-coded number and should use have the same set of numeral placeholders as its plural form.
 
+```js
+// bad
+_n("1 task completed", "{num} tasks completed", n)
+_n("One task completed", "{num} tasks completed", n)
+
+// good
+_n("{num} task completed", "{num} tasks completed", n)
+```
+
+Why: It is incorrect to assume that the "singular" means "n=1" in all languages. In Russian language, its singular form is used for number ending with 1 (i.e. "1, 21, 31, 41, 51, 61, 71, 81, 101, 1001").
+
+When you localize your singular form with number hardcoded in the string, (i.e. "One task completed"), the Russian translator would also provide translation with 'one' encoded in the translation. But this singular translation cannot be used when "n = 21".
+
+
+_What if using placeholder in the singular isn't preferrable_
+
+There will be times when using placeholder in singular might result in a less natural tone.
+
+For instance, maybe we hope to say:
+
+```
+// Ideal for English, for not work for Russian
+_n("Are you sure you want to delete this task?", "Are you sure you want to delete {num} tasks?", n)
+``
+
+In this scenario, you could create separate localized string without plural form dedicated for n = 1
+
+```
+if (n === 1) {
+    return _("Are you sure you want to delete this task?")
+} else {
+    return _n("Are you sure you want to delete {num} task?", "Are you sure you want to delete {num} tasks?", n)
+}
+```
--- index.js
+++ index.js
@@ -20,6 +20,59 @@ function requireStringLiteralArgs({ context, node, name, indexes = [0] } = {}) {
     }
 }
 
+/*
+ * ex:
+ *   parsePlaceholders("{user} completed {num} tasks") // would return {"user", "num"}
+ */
+function parsePlaceholders(str) {
+    var reg = /{([^}]+)}/g
+    var placeholders = new Set()
+
+    let res
+    while ((res = reg.exec(str)) !== null) {
+        placeholders.add(res[1])
+    }
+
+    return placeholders
+}
+
+/*
+ * @param {Set} a
+ * @param {Set} *
+ */
+function difference(a, b) {
+    return new Set([...a].filter(x => !b.has(x)))
+}
+
+function pluralFormPlaceholderCheck({
+    context,
+    node,
+    name,
+    indexes = [0]
+} = {}) {
+    if (node.callee.name !== name) {
+        return
+    }
+
+    const args = node.arguments
+
+    const allPlacholders = indexes.reduce((set, index) => {
+        return new Set([...set, ...parsePlaceholders(args[index].value)])
+    }, new Set())
+
+    indexes.forEach(index => {
+        var arg = args[index]
+        var str = arg.value
+        var placeholders = parsePlaceholders(str)
+        var missing = difference(allPlacholders, placeholders)
+
+        if (missing.size > 0) {
+            var commaList = [...missing].map(s => `"{${s}}"`).join(" ,")
+            context.report(arg, `"${str}" missing placeholder: ${commaList}`)
+        }
+    })
+}
+
 module.exports = {
     rules: {
         "static-path-literal-arg": {
@@ -62,6 +115,26 @@ module.exports = {
                             node,
                             context,
                             indexes: [0, 1, 2]
+                        })
+                    }
+                }
+            }
+        },
+        "gettext-pluralform-placeholder": {
+            create: function(context) {
+                return {
+                    CallExpression(node) {
+                        pluralFormPlaceholderCheck({
+                            name: "_n",
+                            node,
+                            context,
+                            indexes: [0, 1]
+                        })
+                        pluralFormPlaceholderCheck({
+                            name: "_nx",
+                            node,
+                            context,
+                            indexes: [1, 2]
                         })
                     }
                 }
